2f0ce80b8f504437ec39b77f329e6a239f7203ed
filippofinke <filippofinke@protonmail.ch>
2022-02-28 18:31:54 +0100

    📝 feat: update examples

--- tests/main.js
+++ tests/main.js
@@ -1,13 +1,10 @@
 const tutti_api = require("./../src/index");
 
 (async () => {
-
+  console.log(await tutti_api.account.login(process.env.EMAIL, process.env.PASSWORD, true));
   let pr = (await tutti_api.search.find("xbox | ps4")).items;
   pr.forEach((p) => console.log(p.subject));
 
-  console.log(
-    await tutti_api.account.login(process.env.EMAIL, process.env.PASSWORD, true)
-  );
   console.log(await tutti_api.subscriptions.getAvailableBumps());
   console.log(await tutti_api.account.getPaywall());
   console.log(await tutti_api.subscriptions.getPlans());
@@ -20,5 +17,44 @@ const tutti_api = require("./../src/index");
   console.log(await tutti_api.account.getDisabledItems());
   console.log(await tutti_api.account.getArchivedItems());
   console.log(await tutti_api.uploadImage(__dirname + "/test.jpg"));
-  console.log(await tutti_api.account.logout());
+
+  const messageHandler = (messages) => {
+    for (let messageId in messages) {
+      let {
+        id,
+        content: { text },
+        type,
+        senderPublicAccountId,
+        offset,
+        timestamp,
+      } = messages[messageId];
+      console.log(id, text, type, senderPublicAccountId, offset, timestamp);
+    }
+  };
+
+  const conversationHandler = (conversations) => {
+
+    for (let convId in conversations) {
+      let {
+        id,
+        item: { id: itemId, subject },
+        latestMessage: {
+          content: { text },
+          type,
+          timestamp,
+          senderPublicAccountId,
+        },
+      } = conversations[convId];
+      console.log(id, itemId, subject, "\n", text, type, timestamp, senderPublicAccountId, "\n");
+      tutti_api.messaging.setMessageHandler(id, messageHandler);
+    }
+  };
+
+  tutti_api.messaging.setConversationHandler(conversationHandler);
+
+  const statusHandler = ({ conversationsWithUnreadMessages }) => {
+    console.log(`conversationsWithUnreadMessages: ${conversationsWithUnreadMessages}`);
+  };
+
+  tutti_api.messaging.setStatusHandler(statusHandler);
 })();
